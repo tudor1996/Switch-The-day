8040d9d1dc090e8387544ab7725f1793c4a36522
Moldovan Tudor <tmoldovan19@yahoo.com>
2021-03-02 11:24:35 +0200

    Add project files.

---
+++ C1.5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace C1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int zi;
+            Console.Write("Dati ziua:");
+                try
+            {
+                zi = int.Parse(Console.ReadLine());
+                switch (zi)
+                {
+
+                    case 1: Console.WriteLine("Luni"); break;
+                    case 2: Console.WriteLine("Marti"); break;
+                    case 3: Console.WriteLine("Miercuri");break;
+                    case 4: Console.WriteLine("Joi");break;
+                    case 5: Console.WriteLine("Vineri"); break;
+                    case 6: Console.WriteLine("Sambata"); break;
+                    case 7: Console.WriteLine("Duminica"); break;
+                    default: Console.WriteLine("Zi eronata"); break;
+                }
+                Console.ReadLine();
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);           
+            }
+        }
+    }
+}
